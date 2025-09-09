ce645707955079289f596f2cadd0ce91fd08ef16
Azaanul-Abid-Ramadhan <if23.azaanulramadhan@mhs.ubpkarawang.ac.id>
2025-09-09 08:07:15 +0700

    n localSt

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// Simpan status ceklis ke localStorage
-const ceklisKey = "jadwal-ceklis";
-const cekboxes = document.querySelectorAll(".cek-selesai");
-// Load dari localStorage
-function loadCeklis() {
-  const saved = JSON.parse(localStorage.getItem(ceklisKey) || "[]");
-  cekboxes.forEach((cb, i) => {
-    cb.checked = !!saved[i];
-  });
-}
-// Simpan ke localStorage setiap ada perubahan
-cekboxes.forEach((cb, i) => {
-  cb.addEventListener("change", () => {
-    const arr = Array.from(cekboxes).map((c) => c.checked);
-    localStorage.setItem(ceklisKey, JSON.stringify(arr));
-  });
-});
-loadCeklis();
+// // Simpan status ceklis ke localStorage
+// const ceklisKey = "jadwal-ceklis";
+// const cekboxes = document.querySelectorAll(".cek-selesai");
+// // Load dari localStorage
+// function loadCeklis() {
+//   const saved = JSON.parse(localStorage.getItem(ceklisKey) || "[]");
+//   cekboxes.forEach((cb, i) => {
+//     cb.checked = !!saved[i];
+//   });
+// }
+// // Simpan ke localStorage setiap ada perubahan
+// cekboxes.forEach((cb, i) => {
+//   cb.addEventListener("change", () => {
+//     const arr = Array.from(cekboxes).map((c) => c.checked);
+//     localStorage.setItem(ceklisKey, JSON.stringify(arr));
+//   });
+// });
+// loadCeklis();
